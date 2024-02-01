b7044a8fd272b38ca0eb240fe6359060563e6fdf
Christopher Moore <christopher.e.moore@gmail.com>
2024-02-01 12:19:41 +0700

    more

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,18 @@
 
 #include <dlfcn.h>
 #include <iostream>
+#include <thread>	//debugging / checking thread of events vs updates vs init
 
 struct DFCrack {
+	/*
+	ok there's two threads at play here
+	one handle init, quit, event
+	another handles update
+	soo ... give each thread its own lua state?
+	*/
 	LuaCxx::State lua;
 	DFCrack() {
-		std::cout << "DFCrack::DFCrack" << std::endl;
+std::cout << "DFCrack::DFCrack this_thread=" << std::this_thread::get_id() << std::endl;
 		
 		// hmm I could have each function call some custom code
 		// but most like that code will deref into a global table 
@@ -40,10 +47,10 @@ struct DFCrack {
 	}
 	~DFCrack() {
 // trust in __gc methods to clean up?		
-		std::cout << "DFCrack::DFCrack" << std::endl;
+std::cout << "DFCrack::DFCrack this_thread=" << std::this_thread::get_id() << std::endl;
 	}
 	void sdlInit() {
-//		std::cout << "DFCrack::sdlInit" << std::endl;
+std::cout << "DFCrack::sdlInit this_thread=" << std::this_thread::get_id() << std::endl;
 // TODO save the refs for faster access?
 // or TODO verify existence to spare us some repeated errors?
 #if 0		
@@ -59,7 +66,7 @@ struct DFCrack {
 #endif
 	}
 	void sdlQuit() {
-//		std::cout << "DFCrack::sdlQuit" << std::endl;
+std::cout << "DFCrack::sdlQuit this_thread=" << std::this_thread::get_id() << std::endl;
 #if 0		
 		lua["dfc"]["sdlQuit"]();
 #endif
@@ -74,7 +81,7 @@ struct DFCrack {
 	}
 	void update() {
 // too much printing
-//		std::cout << "DFCrack::update" << std::endl;
+std::cout << "DFCrack::update begin this_thread=" << std::this_thread::get_id() << std::endl;
 #if 0		
 		lua["dfc"]["update"]();
 #endif
@@ -86,25 +93,39 @@ struct DFCrack {
 		.call(0, 0)
 		.pop();
 #endif
+std::cout << "DFCrack::update end this_thread=" << std::this_thread::get_id() << std::endl;
 	}
 	bool sdlEvent(SDL_Event * ev) {
-//		std::cout << "DFCrack::sdlEvent" << std::endl;
+std::cout << "DFCrack::sdlEvent begin this_thread=" << std::this_thread::get_id() << std::endl;
 #if 0		
 		return (bool)lua["dfc"]["event"]();
 #endif
 #if 1
-		bool result;
+		bool result = true;		// true default = df handles sdl events
+#if 0// not working?		
 		lua
 		.stack()
 		.getGlobal("dfc")
 		.get("event");
-		lua_pushlightuserdata(lua.getState(), ev);	// hmm is there no way to push a cdata void* onto the stack?
+		//lua_pushlightuserdata(lua.getState(), ev);	// hmm is there no way to push a cdata void* onto the stack?
 		lua
 		.stack()
-		.call(1, 1)
-		.pop(result)
+		.call(0, 0)
+		//.pop(result)
 		.pop();
+#endif		
+#if 1
+std::cout << "lua top " << lua.stack().top() << std::endl;
+		lua.stack()
+		.getGlobal("print")
+		.push("hi")
+		.call(1, 0);
+#endif
+std::cout << "DFCrack::sdlEvent end this_thread=" << std::this_thread::get_id() << std::endl;
 		return result;
+#endif
+#if 0
+		return true;	// false = we handled the event
 #endif
 	}
 };
@@ -141,12 +162,18 @@ DFCRACK_CEXPORT void SDL_Quit() {
 	}
 }
 
+// this is run on a dif thread ... so don't call it.
+// then again ...
+// this is the thread we want ...
+// hmm, maybe I should just have 2 states for each 2 threads
+// and some comm between them?
 DFCRACK_CEXPORT int SDL_NumJoysticks() {
 	crack.update();
 	return 0;	//or -1 on error or something
 }
 
 DFCRACK_CEXPORT int SDL_PollEvent(SDL_Event * const event) {
+//std::cout << "SDL_PollEvent" << std::endl;	
 	if (!event) return 0;
 	int result;
 	while ((result = _SDL_PollEvent(event))) {
